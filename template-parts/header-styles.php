5858efb8c8ff22994082e2a99942bd252acb4263
CrystalUvU <75089133+CrystalUvU@users.noreply.github.com>
2021-03-16 00:21:34 +0500

    i upload this huinya

---
+++ template-parts/header-styles.php
@@ -0,0 +1,51 @@
+<!-- header styles -->
+
+<link id="u-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+<style>.u-header {
+  background-image: none;
+}
+.u-header .u-sheet-1 {
+  min-height: 112px;
+}
+.u-header .u-image-1 {
+  margin: 0 auto 0 0;
+}
+.u-header .u-logo-image-1 {
+  max-width: 112px;
+  max-height: 112px;
+}
+.u-header .u-menu-1 {
+  margin: -58px 0 22px auto;
+}
+.u-header .u-nav-1 {
+  font-size: 1rem;
+  letter-spacing: 0px;
+}
+.u-block-50e0-19 {
+  box-shadow: 2px 2px 8px 0 rgba(128,128,128,1);
+}
+.u-header .u-nav-2 {
+  font-size: 1.25rem;
+}
+.u-block-50e0-17 {
+  box-shadow: 2px 2px 8px 0 rgba(128,128,128,1);
+}
+@media (max-width: 1199px) {
+  .u-header .u-image-1 {
+    width: 64px;
+    height: 32px;
+  }
+  .u-header .u-menu-1 {
+    width: auto;
+  }
+}
+@media (max-width: 767px) {
+  .u-header .u-image-1 {
+    width: auto;
+  }
+}
+@media (max-width: 575px) {
+  .u-header .u-image-1 {
+    width: auto;
+  }
+}</style>
